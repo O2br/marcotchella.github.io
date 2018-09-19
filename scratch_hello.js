9135d1d8b8ce6cd774830471524366a491ba85fc
André da Silva Mesquita <o2br.com@gmail.com>
2018-09-19 17:35:54 -0300

    Create scratch_hello.js

---
+++ scratch_hello.js
@@ -0,0 +1,21 @@
+ (function(ext) {
+        // Cleanup function when the extension is unloaded
+        ext._shutdown = function() {};        // Status reporting code
+        // Use this to report missing hardware, plugin or unsupported browser
+        ext._getStatus = function() {
+            return {status: 2, msg: 'Installed'};
+        };
+        
+        var descriptor = {
+            blocks: [
+                [' ', 'hello world', 'helloWorld']
+            ]
+        };
+        
+        //The Hello world function
+        ext.helloWorld = function(){
+        	alert('Hello World!')
+        };
+        
+        ScratchExtensions.register('Hello World', descriptor, ext);
+    })({});
