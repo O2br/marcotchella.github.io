9b2105ff06bbcc7d9ac129e490fbc63a68b1a75c
André da Silva Mesquita <o2br.com@gmail.com>
2018-11-03 18:42:25 -0300

    test4

--- scratch_hello.js
+++ scratch_hello.js
@@ -1,4 +1,5 @@
  (function(ext) {
+  
         // Cleanup function when the extension is unloaded
         ext._shutdown = function() {};        // Status reporting code
         // Use this to report missing hardware, plugin or unsupported browser
