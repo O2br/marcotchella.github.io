f094a23e5aa6e4f1418c3fe283a03c9fe1205080
André da Silva Mesquita <o2br.com@gmail.com>
2018-11-12 14:35:10 -0200

    Update rgb.js

--- rgb.js
+++ rgb.js
@@ -6,22 +6,23 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
-    ext.set_rgb = function(r,g,b, callback) {
+    ext.set_rgb = function(r,g,b) {
         $.ajax({
-              url: 'http://052f29fb.ngrok.io/api/colordata?r='+r+'&g='+g+'&b='+b,
+              url: 'http://052f29fb.ngrok.io/api/colordata/?r='+r+'&g='+g+'&b='+b,
               dataType: 'get',
               success: function( weather_data ) {
                   // Got the data - parse it and return the temperature
                   temperature = weather_data;
+                  console.log(weather_data);
                   //callback(temperature);
-                  window.setTimeout(function() {callback(temperature);}, espera_comando); //executa e espera o tempo do parametro 
+                  //window.setTimeout(function() {callback(temperature);}, espera_comando); //executa e espera o tempo do parametro 
               }
         });  
     };        
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'Led RGB - R %s G %s B %s', 'set_rgb', '0', '0', '0'],
+            [' ', 'Led RGB - R %s G %s B %s', 'set_rgb', '0', '0', '0'],
         ]
     };
     // Register the extension
