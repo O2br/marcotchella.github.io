751aeb20e59ade9a6fbedcf07c07bd24d639a913
André da Silva Mesquita <o2br.com@gmail.com>
2019-04-25 12:10:12 -0300

    Update rgb.js

--- rgb.js
+++ rgb.js
@@ -4,7 +4,7 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
-    ext.set_rgb = function(r,g,b) {        
+    ext.set_rgb = function(status,r,g,b) {        
         //alert("RGB: "+r+":"+g+":"+b);
         $.ajax({
               url: 'http://192.168.43.23:8080/api/colordata/?r='+r+'&g='+g+'&b='+b,
