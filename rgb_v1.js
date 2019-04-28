cc50b956e1dc69279c3703446b83286de6ceb67e
André da Silva Mesquita <o2br.com@gmail.com>
2019-04-28 11:03:21 -0300

    Update rgb_v1.js

--- rgb_v1.js
+++ rgb_v1.js
@@ -12,11 +12,7 @@
     ext._shutdown = function() {};
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
-    };
-    ext.set_rgb = function(status,r,g,b) {
-        alert(status);
-    };
-    
+    };	
     ext.set_boca = function(msg) {
               console.log(msg);
               $.ajax({
@@ -31,29 +27,24 @@
     ext.set_olho = function(posicao,situacao) {
         if(typeof posicao=="string" && typeof situacao=="string"){
               $.ajax({
-                url: 'http://192.168.2.22:8080/olhp?posicao='+chapeuPosicao[posicao]+'&estado='+ chapeuSituacao[situacao] ,
+                url: 'http://192.168.2.22:8080/olho?posicao='+chapeuPosicao[posicao]+'&estado='+ chapeuSituacao[situacao] ,
                 dataType: 'get',
                 success: function( data ) {
                   console.log(data);
                 }
               }); 		
-	}
-	    
+	}	    
         console.log(posicao+":"+p);
-        console.log(situacao+":"+s);                
-        
-        
+        console.log(situacao+":"+s);                                
     };    
     // Block and block menu descriptions
     var descriptor = {
         menus : {
-            motorPort:["M1","M2"],
             posicao:["direito","esquerdo"],
             situacao:["ligado","desligado"],
             
         } ,
         blocks: [
-            [' ', 'Led RGB %m.motorPort - R %s G %s B %s ', 'set_rgb',' ', '0', '0', '0'],            
             [' ', 'Olho %m.posicao mudar para: %m.situacao ', 'set_olho',' ', ' '],
             [' ', 'Boca mostrar : %s ', 'set_boca',' '],
         ],
